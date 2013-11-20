752e71c8973c7b65ee81addb4facb80ac4031ca1
Connor Boyd <Connor.Boyd@spir3.com>
2013-11-19 17:12:30 -0700

    Initial commit of blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fakeblock::Application.config.secret_key_base = '957a64e60bd543c8559f09fc62227a6c616f9ef1554c9ea4520b796f23117b971f9e0d1f44dd9e2ec346c78a42229667b68925c218499ea140d018fe2d267539'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fakeblock::Application.config.session_store :cookie_store, key: '_Fakeblock_session'
