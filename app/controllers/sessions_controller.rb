9ae9f722065c104bd9104305e34018cb0ebda4f1
Connor Boyd <Connor.Boyd@spir3.com>
2013-11-27 16:33:23 -0700

    Added a logout button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,6 @@ class SessionsController < ApplicationController
   def login_attempt
   	authorized_user = User.authenticate(params[:email], params[:login_password])
   	if authorized_user
-  		p "authorized_user.id = "
-  		p authorized_user.id
   		session[:user_id] = authorized_user.id
   		flash[:notice] = "You are now logged in as #{authorized_user.first_name} #{authorized_user.last_name}"
   		redirect_to(:action => 'profile')
