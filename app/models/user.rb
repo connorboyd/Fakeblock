ce61560ed59a4c7bce421fa842b0d08627b795bf
Connor Boyd <Connor.Boyd@spir3.com>
2013-11-27 15:07:25 -0700

    Registration works, kind of. I don't think it does the password confirmation yet

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,37 @@
 class User < ActiveRecord::Base
-	
+	require 'digest/sha1'
+
+	before_save :encrypt_password
+	after_save :clear_password
+
+	def encrypt_password
+		if password.present?
+			self.password = Digest::SHA1.hexdigest(self.password)
+		end
+	end
+
+	def clear_password
+		self.password = nil
+	end
+
+	def self.authenticate(user_email="", login_password="")
+		user = User.find_by_email(user_email)
+
+		if user && user.match_password(login_password)
+			return user
+		else
+			return false
+		end
+	end
+
+	def match_password(login_password = "")
+		encrypted_password == Digest::SHA1.hexdigest(password)
+	end
+	# encrypted_password= Digest::SHA1.hexdigest(password)
+
+	# attr_accessor :password
+	EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+	validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+	validates :password, :confirmation => true #password_confirmation attr
+	validates_length_of :password, :in => 6..20, :on => :create
 end
