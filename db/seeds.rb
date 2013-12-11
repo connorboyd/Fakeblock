c6e4ed1167031040c32043d376cd55fa56dad6b7
Connor Boyd <Connor.Boyd@spir3.com>
2013-12-11 16:06:14 -0700

    sessions_controller tests working. added some seed data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: "connor.boyd@colorado.edu", first_name: "Connor", last_name: "Boyd", password: "password")
---
+++ spec/models/user_friend_relation_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe UserFriendRelation do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
