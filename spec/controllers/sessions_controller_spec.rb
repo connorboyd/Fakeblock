6c3c66c703c616aa6234b94b215c9ffa74b059e8
Connor Boyd <Connor.Boyd@spir3.com>
2013-12-11 15:09:39 -0700

    Added sessions controller spec

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'sessions_controller'
+
+describe SessionsController do
+
+	describe "Login Attempt" do
+		it "Authenticates the user" do
+			pending
+		end
+	end
+
+	describe "Bad Login Attempt" do
+		it "Does not authenticate the user if the password is wrong" do
+			pending
+		end
+	end
+
+	describe "Logout" do
+		it "Logs the user out" do
+			pending
+		end
+	end
+end
