b05dcd8eb65c503cd11459f215e7b7713cb1ad74
Connor Boyd <Connor.Boyd@spir3.com>
2013-12-11 17:02:17 -0700

    Moved the profile into the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
   def show
   end
 
+  def profile
+    authenticate_user #This is only for the logged in user. It should work with any user
+  end
+
   # GET /users/new
   def new
     @user = User.new
