451e513fb2bfbc4e6c3ab3802fd492e1b10827ad
Connor Boyd <Connor.Boyd@spir3.com>
2013-12-17 17:19:46 -0700

    Redirects to profile after posting - previously redirected to the post, which allowed users to edit their post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.html { redirect_to "/profile?id=#{@post.on_wall_of_user}", notice: 'Post was successfully created.' }
         format.json { render action: 'show', status: :created, location: @post }
       else
         format.html { render action: 'new' }
